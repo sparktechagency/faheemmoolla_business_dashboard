f8f9920a3f9c955bc3930cce5ef60e5f9952f72b
Md. Sohag Hossain <sohaghossain7th@gmail.com>
2025-03-26 20:19:09 +0600

    bug fixed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -18,31 +18,25 @@ import totalRevenue from "./topCards/totalRevenue.png";
 import totalitem from "./topCards/totalItem.png";
 import totalShop from "./topCards/totalShop.png";
 
-import shopImage from "./shop/shop.png";
+import shopImage from "./shop/Shop.png";
 import shopLogo from "./shop/shopIcon.png";
 
 import offer01 from "./offer/offer01.png";
-
 
 // dashboard sidebar icons
 import dashboard from "./icons/dashboard.svg";
 import bank from "./icons/bank.svg";
 import shop from "./icons/shop.svg";
 import earning from "./icons/earning.svg";
-import help from './icons/help.svg';
-import meal from './icons/meal.svg';
-import offer from './icons/offer.svg';
-import order from './icons/order.svg';
-import setting from './icons/setting.svg';
-import password from './icons/password.svg';
-import notification from './icons/notification.svg';
-import privacy from './icons/privacy.svg';
-import profile from './icons/profile.svg';
-
-
-
-
-
+import help from "./icons/help.svg";
+import meal from "./icons/meal.svg";
+import offer from "./icons/offer.svg";
+import order from "./icons/order.svg";
+import setting from "./icons/setting.svg";
+import password from "./icons/password.svg";
+import notification from "./icons/notification.svg";
+import privacy from "./icons/privacy.svg";
+import profile from "./icons/profile.svg";
 
 export {
   profile,
