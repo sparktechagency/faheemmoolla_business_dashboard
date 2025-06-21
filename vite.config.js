a4b2749272cf39c4aca3a28423a76e63a8da2923
pranab56 <pronabhalder53@gmail.com>
2025-06-21 16:57:57 +0600

    all ok

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Make sure the host is correctly set to 0.0.0.0 or localhost
-    port: 5174, // Or use another port if this one is already occupied
-    proxy: {
-      '/api/places': {
-        target: 'https://maps.googleapis.com/maps/api/place',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/places/, ''),
-        secure: true,
-        configure: (proxy, options) => {
-          proxy.on('error', (err, req, res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
-  }
+    host: "145.223.33.60",
+    port: 4173,
+  },
+  build: {
+    sourcemap: false,  // disables source maps for production
+    minify: "esbuild", // faster minifier
+  },
 });
+
